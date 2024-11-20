a4df6223876a2280019d97efdfa82cf88ceca65a
isQHung <testkiller011@gmail.com>
2024-11-20 15:04:58 +0700

    no change

--- MainPrg.cpp
+++ MainPrg.cpp
@@ -1,133 +1,86 @@
 #include "PrecompiledHeader.h"
+
+#include <conio.h>
+
 using namespace std;
 
-void ThreadFunc2(cgame&);
-void ThreadFunc1(cgame&);
-
-//int main() {
-//    cout << "Press any key to start demo: ";
-//    cin.get();
-//    ctool::ShowConsoleCursor(false);
-//    // genSetUpFile();
-//    cgame game;
-//    game.getMap().readMap(0);
-//    game.startGame();
-//    cout << "game";
-//    thread t1(ThreadFunc1, std::ref(game));
-//    thread t2(ThreadFunc2, std::ref(game));
-//    t1.join();
-//    t2.join();
-//
-//}
-//
-//void ThreadFunc2(cgame& cg) {
-//    vector<cpoint> tPlaces = cg.getMap().getTPlaces();
-//    //ctower tw(tPlaces[0]);
-//    ctower tw({ 0,15,0 });
-//    cbullet& bullet = tw.getBullet();
-//    int speed = bullet.getSpeed();
-//    cenemy& e = cg.getEnemy()[0];
-//    
-//   /* bullet.calPath(eCurr);*/
-//    int i = 0;
-//    cout << "threat 2";
-//    while (!cg.getIsExist1()) {
-//        cpoint eCurr = e.getCurr();
-//        tw.draw();
-//        if(bullet.getCurr().distance(e.getCurr()) < 2){
-//            cout << "hit";
-//            cg.setIsExist1(true);
-//            break;
-//        }else{
-//
-//            bullet.draw(' ');
-//            bullet.calPath(e.getCurr());
-//            bullet.draw('*');
-//            i++;
-//        }
-//        Sleep(1000 / bullet.getSpeed());
-//    }
-//    cg.setIsExist2(true);
-//}
-//
-//void ThreadFunc1(cgame& cg) {
-//    vector<cpoint> ePath = cg.getMap().getEPath(0);
-//   /* vector<cpoint> directionX = cg.getMap().getEPath(0);
-//    vector<cpoint> directionX = cg.getMap().getEPath(0);*/
-//    int i = 0;
-//    cenemy& e = cg.getEnemy()[0];
-//    e.calPath(ePath);
-//
-//    while (!cg.getIsExist1()) {
-//        if (e.isEnd()) break;
-//        e.draw();
-//        Sleep(1000 / e.getSpeed());
-//    }
-//    cg.setIsExist1(true);
-//}
+mutex mtx;
+condition_variable cv;
+bool pause = false;
+bool phase = false;
 
 //Game logic
-int game(int);
+int StartGame(int, cplayer&);
 int placeTower(cgame&);
 void enemyThread(cgame&, cenemy&);
 void towerThread(cgame&, ctower);
 void bulletThread(cbullet&, cenemy&);
 void checkAvaibleEnemy(cgame&);
+void pauseGameMenu(cgame& game);
 
-int game(int numMap) {
-    ctool::ShowConsoleCursor(0);
+int StartGame(int numMap, cplayer& player) {
+    //ctool::ShowConsoleCursor(0);
+
     cgame game;
+    game.setPlayer(player);
     cmap &map = game.getMap();
-
+    
     //game.startGame();
     map.readMap(numMap);
+
     //place tower
-    placeTower(game);
+    //placeTower(game);
+
+    game.readFile("testSaveGame");
+
+
     //get Enemy
     int NumOfPath = map.getNumOfPath();
     vector<cenemy>& eList = game.getEnemy();
+    /*for (auto e : eList) {
+        ctool::GotoXY(1, 1);
+        cout << 1;
+    }
     int nEnemy[4], AllEnemy = 0;
+
     for (int i = 0; i < NumOfPath; i++) {
         nEnemy[i] = map.getEnemy(i);
         AllEnemy += nEnemy[i];
     }
+
     for (int i = 0; i < AllEnemy; i++) {
         eList.push_back(cenemy());
         eList[i].calPath(map.getEPath(i%NumOfPath));
-    }
-
+    }*/
     //start thread
+
     vector<ctower>& towerList = game.getTower();
-    //vector<thread> towerThreadList;
+
     vector<thread> threads;
+
+    threads.push_back(thread(pauseGameMenu, ref(game)));
     for (int i = 0; i < towerList.size(); i++) {
-        //towerThreadList.push_back(thread(towerThread, ref(game), towerList[i]));
         threads.push_back(thread(towerThread, ref(game), towerList[i]));
         
     }
-    //vector<thread> enemyThreadList;
+
     for (int i = 0; i < eList.size(); i++) {
-        //enemyThreadList.push_back(thread(enemyThread, ref(game), ref(eList[i])));
+        unique_lock<mutex> lock{ mtx };
+        cv.wait(lock, []() {return !pause; });
         threads.push_back(thread(enemyThread, ref(game), ref(eList[i])));
+        lock.unlock();
+        this_thread::sleep_for(chrono::milliseconds(1000 * 2));
+        //threads.push_back(thread(enemyThread, ref(game), ref(eList[i])));
         this_thread::sleep_for(chrono::milliseconds(1000*2));
     }
+
     threads.push_back(thread(checkAvaibleEnemy, ref(game)));
+
     //join thread
     for (thread& t : threads) {
         if (t.joinable())
             t.join();
     }
-
-    /*for(int i = 0; i < towerThreadList.size(); i++){
-        if(towerThreadList[i].joinable())
-            towerThreadList[i].join();
-    }*/
-    
-    /*for (int i = 0; i < enemyThreadList.size(); i++) {
-        if (enemyThreadList[i].joinable())
-            enemyThreadList[i].join();
-        Sleep(1000 / 2);
-    }*/
     return 0;
 }
 
@@ -141,17 +94,18 @@ int placeTower(cgame &game){
 }
 
 void towerThread(cgame& game,ctower tower){
-    /*ctool::mtx.lock();
-    cout << this_thread::get_id() << endl;
-    cout << tower.getLocation().getX() << endl;
-    ctool::mtx.unlock();*/
     tower.draw();
     vector<cenemy>& eList = game.getEnemy();
+
     while (!game.getIsExist1()) {
+
         for (int i = 0; i < eList.size(); i++) {
+
             if (tower.getLocation().distance(eList[i].getCurr()) < tower.getRange() && eList[i].getHealth() > 0) {
+
                 cbullet& bullet = tower.getBullet();
                 bulletThread(bullet, eList[i]);
+
                 this_thread::sleep_for(chrono::milliseconds(500 * tower.getSpeed()));
                 break;
             }
@@ -161,12 +115,17 @@ void towerThread(cgame& game,ctower tower){
 
 void bulletThread(cbullet& bullet, cenemy& enemy) {
     int bullbetSpeed = bullet.getSpeed();
-    while (bullet.getCurr().distance(enemy.getCurr()) > 2) {
+
+    while (bullet.getCurr().distance(enemy.getCurr()) > 2 && enemy.getHealth() > 0) {
+        unique_lock<mutex> lock{ mtx };
+        cv.wait(lock, []() {return !pause; });
         bullet.draw(' ');
         bullet.calPath(enemy.getCurr());
         bullet.draw('*');
+        lock.unlock();
         Sleep(200 / bullbetSpeed); 
     }
+
     enemy.hit();
     bullet.draw(' ');
     enemy.draw();
@@ -175,38 +134,99 @@ void bulletThread(cbullet& bullet, cenemy& enemy) {
 void enemyThread(cgame& game, cenemy& enemy) {
 
     int enemySpeed = enemy.getSpeed();
+
     while (!game.getIsExist1()) {
+
+        unique_lock<mutex> lock{ mtx };
+        cv.wait(lock, []() {return !pause; });
+
         if (enemy.isEnd()) {
             game.setIsExist1(true);
             break;
         }
+
         if (enemy.getHealth() <= 0) break;
+
         enemy.draw();
+
+        lock.unlock();
         this_thread::sleep_for(chrono::milliseconds(500 / enemy.getSpeed()));
     }
+    game.addPoint(10);
     //game.setIsExist1(true);
 }
 
 void checkAvaibleEnemy(cgame& game) {
     vector<cenemy>& eList = game.getEnemy();
+
     while (!game.getIsExist1()) {
         ctool::mtx.lock();
         int count = 0;
+
         for (cenemy& e : eList) {
+
             if (e.getHealth() > 0) {
                 count++;
             }
+
         }
+
         if (count == 0) break;
+        //if (count < game.getEnemy().size()/2) 
         ctool::mtx.unlock();
     }
     game.setIsExist1(true);
+    //game.saveGame("testSavegame");
+}
+
+void pauseGameMenu(cgame& game) {
+    while (!game.getIsExist1()) {
+        //listenEvent
+        while (!pause) {
+            int c = _getch();
+            if (c == 27 || c == 80) {
+                std::unique_lock<std::mutex> lock{ ctool::mtx };
+                pause = true;
+                cv.notify_all();
+            }
+        }
+        //set player name
+        //show popup screen
+        game.saveGame("testSaveGame");
+        int index = 0;
+        while (pause) {
+            int ch = _getch();
+            if (ch == KEY_ARROW_PREFIX) {
+                ch = _getch();
+                if (ch == KEY_LEFT) {
+                    index--;
+                }
+                else if (ch == KEY_RIGHT) {
+                    index++;
+                }
+                if (index == -1) index = 1;
+                if (index == 2) index = 0;
+            }
+            else if (ch == KEY_ENTER) {
+                //if (index == 1) screenCode = "main screen"; // den Main screen
+                //else game.setIsExist2(false); // tiep tuc choi
+            }
+            else if (ch == KEY_ESC) {
+                game.setIsExist2(false);
+                pause = false;
+                cv.notify_all();
+            }
+            //continue;
+            //Sleep(500);
+        }
+        //lock.unlock();
+    }
 }
 
 int main() {
     //genSetUpFile();
-
-    game(1); // man choi 1
-    //game(2); // man choi 2
+    cplayer player;
+    StartGame(1, player); // man choi 1
+    //StartGame(2, player); // man choi 2
     return 0;
 }
--- PrecompiledHeader.h
+++ PrecompiledHeader.h
@@ -6,3 +6,92 @@
 
 #include <windows.h>
 #include <TowerDefense.h>
+
+// Alphabet A-Z
+#define KEY_A 65
+#define KEY_B 66
+#define KEY_C 67
+#define KEY_D 68
+#define KEY_E 69
+#define KEY_F 70
+#define KEY_G 71
+#define KEY_H 72
+#define KEY_I 73
+#define KEY_J 74
+#define KEY_K 75
+#define KEY_L 76
+#define KEY_M 77
+#define KEY_N 78
+#define KEY_O 79
+#define KEY_P 80
+#define KEY_Q 81
+#define KEY_R 82
+#define KEY_S 83
+#define KEY_T 84
+#define KEY_U 85
+#define KEY_V 86
+#define KEY_W 87
+#define KEY_X 88
+#define KEY_Y 89
+#define KEY_Z 90
+
+// Numbers 0-9
+#define KEY_0 48
+#define KEY_1 49
+#define KEY_2 50
+#define KEY_3 51
+#define KEY_4 52
+#define KEY_5 53
+#define KEY_6 54
+#define KEY_7 55
+#define KEY_8 56
+#define KEY_9 57
+
+// Special Characters
+#define KEY_SPACE 32
+#define KEY_EXCLAMATION 33    // !
+#define KEY_AT 64             // @
+#define KEY_HASH 35           // #
+#define KEY_DOLLAR 36         // $
+#define KEY_PERCENT 37        // %
+#define KEY_CARET 94          // ^
+#define KEY_AMPERSAND 38      // &
+#define KEY_ASTERISK 42       // *
+#define KEY_LEFT_PAREN 40     // (
+#define KEY_RIGHT_PAREN 41    // )
+#define KEY_MINUS 45          // -
+#define KEY_UNDERSCORE 95     // _
+#define KEY_PLUS 43           // +
+#define KEY_EQUAL 61          // =
+#define KEY_LEFT_BRACKET 91   // [
+#define KEY_RIGHT_BRACKET 93  // ]
+#define KEY_LEFT_CURLY 123    // {
+#define KEY_RIGHT_CURLY 125   // }
+#define KEY_SEMICOLON 59      // ;
+#define KEY_COLON 58          // :
+#define KEY_QUOTE 39          // '
+#define KEY_DOUBLE_QUOTE 34   // "
+#define KEY_COMMA 44          // ,
+#define KEY_PERIOD 46         // .
+#define KEY_SLASH 47          // /
+#define KEY_BACKSLASH 92      // \
+#define KEY_PIPE 124          // |
+#define KEY_LESS_THAN 60      // <
+#define KEY_GREATER_THAN 62   // >
+#define KEY_QUESTION 63       // ?
+#define KEY_TILDE 126         // ~
+#define KEY_GRAVE 96          // `
+
+// Control Keys
+#define KEY_ESC 27            // Escape
+#define KEY_ENTER 13          // Enter
+#define KEY_BACKSPACE 8       // Backspace
+#define KEY_TAB 9             // Tab
+#define KEY_DELETE 127        // Delete
+
+// Arrow Keys (use _getch() to read twice in Windows)
+#define KEY_ARROW_PREFIX 224  // Prefix for arrow keys
+#define KEY_UP 72             // Arrow Up
+#define KEY_DOWN 80           // Arrow Down
+#define KEY_LEFT 75           // Arrow Left
+#define KEY_RIGHT 77          // Arrow Right
