cf33a6f59c633a542bc5927877040afde45fb85f
duckilot <prto2802@gmail.com>
2024-12-23 03:59:09 +0700

    merge bug load

--- Game/Screen/MainScreen.cpp
+++ Game/Screen/MainScreen.cpp
@@ -89,6 +89,8 @@ namespace towerdefense
 
             _FourLeaderBoardItem[i] = std::make_shared<ContinueElement>(name, point, level, optionPositionsEnd[i], customfont, RGB(255, 255, 255), maplink[LeaderboardList[i].getMapCode()], 3);
         }
+
+
     }
 
     // Destructor
@@ -238,7 +240,7 @@ namespace towerdefense
             for (int i = 0; i < LoadList.size(); i++) {
                 if (_FourContinueItem[i]->isClicked(cursorPos)) {
                     PostMessage(hwnd, WM_CUSTOM_LOAD_SCREEN, LoadList[i].getMapCode() + 4, 0);
-                    OutputDebugStringA(("map code: " + to_string(LoadList[i].getMapCode() + 4) + "\n").c_str());
+                    //OutputDebugStringA(("map code: " + to_string(LoadList[i].getMapCode() + 4) + "\n").c_str());
                 }
 
             }
